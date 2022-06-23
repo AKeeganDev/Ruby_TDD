7e4a4b3b8fb1a60986fda31cf68f99b47bba4b4d
Alexr16 <j.alex-1996@hotmail.com>
2022-06-23 15:14:39 -0500

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,22 +1,25 @@
 class Solver
-  def factorial (number)
-    return 'invalid number' if number < 0
-    return 1 if number == 0
-    sum = 0;
+  def factorial(number)
+    return 'invalid number' if number.negative?
+    return 1 if number.zero?
+
+    sum = 0
     num_array = *(1..number)
-    num_array.each{|number| sum += number}
+    num_array.each { |num| sum += num }
     sum
   end
 
   def reverse(string)
-    return 'Invalid input' unless string.kind_of?(String)
-    string.reverse        
+    return 'Invalid input' unless string.is_a?(String)
+
+    string.reverse
   end
 
-  def fizzbuzz(n)
-    return 'fizzbuzz' if (n % 5 == 0 and n % 3 == 0)
-    return 'fizz' if (n % 3 == 0)
-    return 'buzz' if (n % 5 == 0)
-    "#{n}"
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 5).zero? and (number % 3).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,41 +2,39 @@ require_relative 'spec_helper'
 
 RSpec.describe Solver do
   # context 'checks to make sure that Solver is initialized' do
- 
+
   # end
   context 'Testing the factorial method' do
-
     it 'Checks that the factorial method can receive a value' do
       mock = double('solver')
-      allow(mock).to receive(:factorial).with (0)
+      allow(mock).to receive(:factorial).with(0)
 
-      solver = Solver.new()
+      solver = Solver.new
       solver.factorial(1)
     end
 
-    it "Checks that the factorial method only works with positive numbers including 0" do
-      solver = Solver.new()
+    it 'Checks that the factorial method only works with positive numbers including 0' do
+      solver = Solver.new
       expect(solver.factorial(-1)).to eql 'invalid number'
     end
 
-    it "Checks that the factorial method identifies 0 as an argument and returns 1" do
-      solver = Solver.new()
+    it 'Checks that the factorial method identifies 0 as an argument and returns 1' do
+      solver = Solver.new
       expect(solver.factorial(0)).to eql 1
     end
 
-    it "Checks that the factorial method returns the correct factorial" do
-      solver = Solver.new()
+    it 'Checks that the factorial method returns the correct factorial' do
+      solver = Solver.new
       expect(solver.factorial(2)).to eql 3
     end
-
   end
 
   context 'Testing the reverse method' do
     it 'Checks that the reverse method can receive a value' do
       mock = double('solver')
-      allow(mock).to receive(:reverse).with ('string')
+      allow(mock).to receive(:reverse).with('string')
 
-      solver = Solver.new()
+      solver = Solver.new
       solver.reverse('string')
     end
 
@@ -54,9 +52,9 @@ RSpec.describe Solver do
   context 'Testing the fizzbuzz method' do
     it 'Checks that the fizzbuzz method can receive a value' do
       mock = double('solver')
-      allow(mock).to receive(:fizzbuzz).with (1)
+      allow(mock).to receive(:fizzbuzz).with(1)
 
-      solver = Solver.new()
+      solver = Solver.new
       solver.fizzbuzz(1)
     end
 
