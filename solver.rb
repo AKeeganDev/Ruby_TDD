dea70bce1ac7f5a6edaf6cde51ff67fe7f0b01cb
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-06-23 14:30:50 -0400

    full factorial test passed

--- solver.rb
+++ solver.rb
@@ -2,5 +2,9 @@ class Solver
   def factorial (number)
     return 'invalid number' if number < 0
     return 1 if number == 0
+    sum = 0;
+    num_array = *(1..number)
+    num_array.each{|number| sum += number}
+    sum
   end
 end
